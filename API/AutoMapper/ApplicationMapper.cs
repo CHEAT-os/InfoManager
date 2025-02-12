b51ba1787d2ed908a9cbf71210a882bc7e80af98
PaulaRioDelgado <163323519+PaulaRio@users.noreply.github.com>
2025-02-13 00:23:23 +0100

    DTO y carpetas añadidas

---
+++ API/AutoMapper/ApplicationMapper.cs
@@ -0,0 +1,20 @@
+﻿
+using AutoMapper;
+
+
+namespace API.AutoMapper
+{
+    public class ApplicationMapper : Profile
+    {
+        public ApplicationMapper()
+        {
+            
+
+            //CreateMap<HouseEntity, HouseDTO>().ReverseMap();
+            //CreateMap<CreateHouseDTO, HouseEntity>().ReverseMap();
+
+
+            //CreateMap<AppUser, UserDto>().ReverseMap();
+        }
+    }
+}
---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using API.Models.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        //Add models here
+      
+        //public DbSet<HouseEntity> Houses { get;set; }
+
+    }
+}
