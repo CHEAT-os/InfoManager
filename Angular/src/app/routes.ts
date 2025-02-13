a44a19a36fe682424496c811b5a822dd422bf527
LuisTorcal99 <luistor99@gmail.com>
2025-02-13 21:27:35 +0100

    #15 View HTML enviar propuesta

--- Angular/src/app/routes.ts
+++ Angular/src/app/routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FormularioComponent } from './pages/formulario/formulario.component';
+
 
 const routeConfig: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home page',
+  },
+  {
+    path: 'formulario',
+    component: FormularioComponent,
+    title: 'formulario',
   },
   {
     path: '**',
