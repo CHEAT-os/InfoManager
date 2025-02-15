094e9aa68d43af4f2bbe6ae1a83c3c4841fbcf86
alejandrofljo <alejandrofljo@proton.me>
2025-02-15 19:04:25 +0100

    16-h2-api-propuesta - Eliminar IDs de CreatePropuesta

--- API/Models/DTOs/Propuesta/CreatePropuestaDTO.cs
+++ API/Models/DTOs/Propuesta/CreatePropuestaDTO.cs
@@ -4,13 +4,11 @@ namespace API.Models.DTOs.Propuesta
 {
     public class CreatePropuestaDTO : PropuestaDTO
     {
-        [Required(ErrorMessage = "Field required: Id")]
-        public int Id { get; set; }
-        [Required(ErrorMessage = "Field required: UserId")]
-        public string UserId { get; set; }
+        [Required(ErrorMessage = "Field required: username")]
+        public string username { get; set; }
         [Required(ErrorMessage = "Field required: Titulo")]
         public string Titulo { get; set; }
-        [Required(ErrorMessage = "Field required: UserId")]
+        [Required(ErrorMessage = "Field required: Descripcion")]
         public string Descripcion { get; set; }
         [Required(ErrorMessage = "Field required: Tipo")]
         public string Tipo { get; set; }
