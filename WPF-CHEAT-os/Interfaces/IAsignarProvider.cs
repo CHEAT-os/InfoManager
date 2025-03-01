40688b57dfe6f875b179eef63a13f18e2c89b746
Luis Torcal <luistor99@gmail.com>
2025-03-01 13:20:29 +0100

    Interface rellena

--- WPF-CHEAT-os/Interfaces/IAsignarProvider.cs
+++ WPF-CHEAT-os/Interfaces/IAsignarProvider.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPF_CHEAT_os.DTO;
 
 namespace WPF_CHEAT_os.Interfaces
 {
     public interface IAsignarProvider
     {
-
+        List<AlumnoDTO> ListaClean(List<AlumnoDTO> listaAprobados, List<AlumnoDTO> ListaAsignada);
+        List<AlumnoDTO> AlumnosAsignados(List<AlumnoDTO> listaAprobados, int maxAlumnos);
+        int TutorPuedeLlevar(int horas, int alumnos);
     }
 }
